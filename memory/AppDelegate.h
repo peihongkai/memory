13a9805bf97db27a4de39c95523dcbb951ac1016
裴洪凯 <peihongkai@peimatoMacBook-Air.local>
2012-11-04 13:00:10 +0800

    Initial Commit

---
+++ memory/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  memory
+//
+//  Created by 裴洪凯 on 12-11-4.
+//  Copyright (c) 2012年 裴洪凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
